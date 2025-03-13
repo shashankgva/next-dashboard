c657a29052e0214333a806046be14d4b4e934f2b
shashankgva <shashankgva@gmail.com>
2025-03-13 13:03:40 +0530

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const CustomersPage = () => {
+  return <h1>Customer Page</h1>;
+};
+
+export default CustomersPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const InvoicesPage = () => {
+  return <h1>Invoices Page</h1>;
+};
+
+export default InvoicesPage;
